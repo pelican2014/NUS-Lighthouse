eed29299982efd5c79a11ff5a7e29803313f6560
Tongyao <312404930zhu@gmail.com>
2019-03-24 14:53:35 +0800

    added internship homepage component

---
+++ src/components/internship-homepage/internship-homepage.vue
@@ -0,0 +1,12 @@
+<template src = './internship-homepage.html'></template>
+<style src = './internship-homepage'></style>
+<script>
+export default{
+	name:'InternshipHomepage',
+	props:{
+		data:{
+			type:Object,
+			required:true
+		},
+	},
+}
