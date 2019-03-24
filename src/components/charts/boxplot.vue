b4dfa26103f87398f63b7e2963ac9106647dae8d
JacobWangTengda <wtd8207@gmail.com>
2019-03-24 17:01:51 +0800

    add props to charts

--- src/components/charts/boxplot.vue
+++ src/components/charts/boxplot.vue
@@ -9,84 +9,95 @@ import Highcharts from 'highcharts';
 
 More(Highcharts);
 
-const data = {
-  chart: {
-    type: 'boxplot',
-  },
-
-  title: {
-    text: 'Highcharts Box Plot Example',
-  },
-
-  legend: {
-    enabled: false,
-  },
-
-  xAxis: {
-    categories: ['1', '2', '3', '4', '5'],
-    title: {
-      text: 'Experiment No.',
-    },
-  },
-
-  yAxis: {
-    title: {
-      text: 'Observations',
-    },
-    plotLines: [{
-      value: 932,
-      color: 'red',
-      width: 1,
-      label: {
-        text: 'Theoretical mean: 932',
-        align: 'center',
-        style: {
-          color: 'gray',
-        },
-      },
-    }],
-  },
-
-  series: [{
-    name: 'Observations',
-    data: [
-      [760, 801, 848, 895, 965],
-      [733, 853, 939, 980, 1080],
-      [714, 762, 817, 870, 918],
-      [724, 802, 806, 871, 950],
-      [834, 836, 864, 882, 910],
-    ],
-    tooltip: {
-      headerFormat: '<em>Experiment No {point.key}</em><br/>',
-    },
-  }, {
-    name: 'Outlier',
-    color: Highcharts.getOptions().colors[0],
-    type: 'scatter',
-    data: [ // x, y positions where 0 is the first category
-      [0, 644],
-      [4, 718],
-      [4, 951],
-      [4, 969],
-    ],
-    marker: {
-      fillColor: 'white',
-      lineWidth: 1,
-      lineColor: Highcharts.getOptions().colors[0],
-    },
-    tooltip: {
-      pointFormat: 'Observation: {point.y}',
-    },
-  }],
-};
-
 export default {
   components: {
     HighchartsVue,
   },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    xname: {
+      type: String,
+      required: true,
+    },
+    xcategory: {
+      type: Array,
+      required: true,
+    },
+    yname: {
+      type: String,
+      required: true,
+    },
+    mean: {
+      type: Number,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+    outliers: {
+      type: Array,
+    },
+  },
   data() {
     return {
-      options: data,
+      options: {
+        chart: {
+          type: 'boxplot',
+        },
+        title: {
+          text: this.title,
+        },
+        legend: {
+          enabled: false,
+        },
+        xAxis: {
+          categories: this.xcategory,
+          title: {
+            text: this.xname,
+          },
+        },
+        yAxis: {
+          title: {
+            text: this.yname,
+          },
+          plotLines: [{
+            value: this.mean,
+            color: 'red',
+            width: 1,
+            label: {
+              text: 'Theoretical mean:',
+              align: 'center',
+              style: {
+                color: 'gray',
+              },
+            },
+          }],
+        },
+
+        series: [{
+          name: this.yname,
+          data: this.data,
+          tooltip: {
+            headerFormat: '<em>Experiment No {point.key}</em><br/>',
+          },
+        }, {
+          name: 'Outliers',
+          color: Highcharts.getOptions().colors[0],
+          type: 'scatter',
+          data: this.outliers,
+          marker: {
+            fillColor: 'white',
+            lineWidth: 1,
+            lineColor: Highcharts.getOptions().colors[0],
+          },
+          tooltip: {
+            pointFormat: 'Observation: {point.y}',
+          },
+        }],
+      },
     };
   },
 };
---
+++ src/components/charts/histogram.vue
@@ -0,0 +1,81 @@
+<template>
+  <highcharts :options="options" ref="bell-curve" class="histogram"></highcharts>
+</template>
+
+<script>
+import HighchartsVue from 'highcharts-vue';
+import Highcharts from 'highcharts';
+import loadHistogram from 'highcharts/modules/histogram-bellcurve';
+
+loadHistogram(Highcharts);
+
+export default {
+  components: {
+    HighchartsVue,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      options: {
+        title: {
+          text: this.title,
+        },
+        xAxis: [{
+          title: { text: this.name },
+          alignTicks: false,
+        }, {
+          title: { text: 'Histogram' },
+          alignTicks: false,
+          opposite: true,
+        }],
+
+        yAxis: [{
+          title: { text: this.name },
+        }, {
+          title: { text: 'Histogram' },
+          opposite: true,
+        }],
+
+        series: [{
+          name: 'Histogram',
+          type: 'histogram',
+          xAxis: 1,
+          yAxis: 1,
+          baseSeries: 's1',
+          zIndex: -1,
+        }, {
+          name: this.name,
+          type: 'scatter',
+          data: this.data,
+          id: 's1',
+          marker: {
+            radius: 1.5,
+          },
+        }],
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.line{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+}
+</style>
