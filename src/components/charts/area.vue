79b5dda87d3e64990905dc5d47a63db393eba53b
Yang Xiya <xiya@u.nus.edu>
2019-03-25 00:04:59 +0800

    change charts

--- src/components/charts/area.vue
+++ src/components/charts/area.vue
@@ -72,6 +72,7 @@ export default {
         },
         plotOptions: {
           area: {
+            stacking: 'normal',
             marker: {
               enabled: false,
               symbol: 'circle',
--- src/components/industry-explorer/industry-explorer.vue
+++ src/components/industry-explorer/industry-explorer.vue
@@ -7,6 +7,8 @@ import AreaChart from '@/components/charts/area';
 import HorizontalBarChart from '@/components/charts/horizontal-bar';
 import Boxplot from '@/components/charts/boxplot';
 
+import db from '@/firebase';
+
 export default {
   name: 'IndustryExplorer',
   components: {
@@ -14,6 +16,17 @@ export default {
     AreaChart,
     HorizontalBarChart,
     Boxplot,
+  },
+  computed: {
+    // internship() {
+    //   return this.internship;
+    // }
+  },
+  firebase: {
+    internship: {
+      source: db.ref('internship'),
+      asArray: true,
+    },
   },
 };
 
