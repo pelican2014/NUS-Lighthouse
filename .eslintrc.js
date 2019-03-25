f0cae49432e6a04f6551b14134ec53d90e137baf
Yang Xiya <xiya@u.nus.edu>
2019-03-25 19:27:49 +0800

    ignore dot notation eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     'no-continue': 0,
     'no-mixed-spaces-and-tabs': 0,
     'no-tabs': 0,
+    'dot-notation': 0,
 
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
