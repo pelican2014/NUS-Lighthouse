2b814d90ab097b7a436bdbc7542455e79fdf02e8
zhirui <zhangzhirui117@gmail.com>
2019-03-25 14:24:29 +0800

    Add component for prof and project page

---
+++ src/components/prof-project-page/prof-project-page.vue
@@ -0,0 +1,8 @@
+<template src="./prof-project-page.html"></template>
+<!-- <style src="./prof-project-page.scss" lang="scss"></style> -->
+
+<script>
+import db from '../../firebase/firebase';
+
+
+</script>
