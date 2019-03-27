c8c6ec83f26b843f335422f1109b6767f72cc62e
Yang Xiya <xiya@u.nus.edu>
2019-03-28 03:43:59 +0800

    add new function for firebase lambda

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -7,9 +7,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 async function handler(change) {
-  const internship = await functions.database.ref('/internship').once('value');
-  return await functions.database.ref('/internship2').set(internship);
+  const root = change.after.ref.parent.parent;
+  const internship_snapshot = await root.child('internship').once('value');
+  const internship = company_snapshot.val();
+  return root.child('/internship').set(internship);
 }
 
 // Keeps track of the length of the 'likes' child list in a separate property.
-exports.internshipCharts = functions.database.ref('/internship').onWrite(handler);
+exports.internshipCharts = functions.database.ref('/internship/updated').onWrite(handler);
