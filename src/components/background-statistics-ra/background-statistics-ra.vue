c6ab7dcc717517828c89bc1871c1c75d7fde5297
zhirui <zhangzhirui117@gmail.com>
2019-03-29 17:42:32 +0800

    Add treemap

--- src/components/background-statistics-ra/background-statistics-ra.vue
+++ src/components/background-statistics-ra/background-statistics-ra.vue
@@ -4,10 +4,13 @@
 <script>
 import _ from 'lodash';
 import db from '@/firebase';
+import Color from 'color';
+import Math from 'mathjs';
 import PieChart from '@/components/charts/pie';
 import BarChart from '@/components/charts/bar';
 import LineChart from '@/components/charts/line';
 import Histogram from '@/components/charts/histogram';
+import Treemap from '@/components/charts/treemap';
 
 
 export default {
@@ -17,6 +20,7 @@ export default {
     BarChart,
     LineChart,
     Histogram,
+    Treemap,
   },
   props: {
     prof_id: {
@@ -32,7 +36,7 @@ export default {
     totalNum() {
       let counter = 0;
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') { // change to prof id
+        if (this.ra_dict[item_id].prof_id === this.prof_id) { // change to prof id
           counter += 1;
         }
       }
@@ -55,7 +59,7 @@ export default {
       let female = 0;
       let male = 0;
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') { // change to prof id
+        if (this.ra_dict[item_id].prof_id === this.prof_id) { // change to prof id
           const ra = this.ra_dict[item_id];
           if (ra.gender === 'F') {
             female += 1;
@@ -73,7 +77,7 @@ export default {
       let prev;
       const majors = [];
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') {
+        if (this.ra_dict[item_id].prof_id === this.prof_id) {
           const ra = this.ra_dict[item_id];
           majors.push(ra.major);
         }
@@ -103,7 +107,7 @@ export default {
       let prev;
       const race_list = [];
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') { // change to prof id
+        if (this.ra_dict[item_id].prof_id === this.prof_id) { // change to prof id
           const ra = this.ra_dict[item_id];
           if (ra.race !== '') {
             race_list.push(ra.race);
@@ -134,7 +138,7 @@ export default {
       let prev;
       const country_list = [];
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') { // change to prof id
+        if (this.ra_dict[item_id].prof_id === this.prof_id) { // change to prof id
           const ra = this.ra_dict[item_id];
           if (ra.nationality !== '') {
             country_list.push(ra.nationality);
@@ -165,7 +169,7 @@ export default {
       let prev;
       const year_list = [];
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') { // change to prof id
+        if (this.ra_dict[item_id].prof_id === this.prof_id) { // change to prof id
           const ra = this.ra_dict[item_id];
           year_list.push(ra.year);
         }
@@ -197,7 +201,7 @@ export default {
     cap_hist() {
       const cap_list = [];
       for (const item_id in this.ra_dict) {
-        if (this.ra_dict[item_id].prof_id === 'p1') {
+        if (this.ra_dict[item_id].prof_id === this.prof_id) {
           const ra = this.ra_dict[item_id];
           cap_list.push(ra.cap);
         }
@@ -226,6 +230,67 @@ export default {
         }
       });
       return counts;
+    },
+    major_faculty() {
+      const child_list = [];
+      const faculty_list = [];
+      const major_list = [];
+      const id_list = [];
+      for (const item_id in this.ra_dict) {
+        if (this.ra_dict[item_id].prof_id === this.prof_id) {
+          id_list.push(item_id);
+          const ra = this.ra_dict[item_id];
+          if (!major_list.includes(ra.major)) {
+            major_list.push(ra.major);
+          }
+          if (!faculty_list.includes(ra.faculty)) {
+            faculty_list.push(ra.faculty);
+          }
+        }
+      }
+
+      for (let i = 0; i < faculty_list.length; i += 1) {
+        const fac = faculty_list[i];
+        for (let j = 0; j < major_list.length; j += 1) {
+          const major = major_list[j];
+          let freq = 0;
+          for (let k = 0; k < id_list.length; k += 1) {
+            const id_faculty = this.ra_dict[id_list[k]].faculty;
+            const id_major = this.ra_dict[id_list[k]].major;
+            if ((id_faculty === fac) && (id_major === major)) {
+              freq += 1;
+            }
+          }
+          if (freq !== 0) {
+            child_list.push({ name: major, parent: fac, value: freq });
+          }
+        }
+      }
+
+      const color_list = ['#4e3a96', '#0e75bc', '#f89520', '#9c5252', '#685343', '#175742', '#ecb605', '#fbd0bf', '#7e8eff'];
+      const result = [];
+      for (let i = 0; i < faculty_list.length; i += 1) {
+        const fac = faculty_list[i];
+        const major = [];
+        const values = [];
+        const random_color = color_list[Math.floor(Math.random() * color_list.length)];
+        result.push({ id: faculty_list[i], name: faculty_list[i] });
+        for (let j = 0; j < child_list.length; j += 1) {
+          if (child_list[j].parent === fac) {
+            major.push(child_list[j].name);
+            values.push(child_list[j].value);
+          }
+        }
+        const freq_min = Math.min(values);
+        const freq_max = Math.max(values);
+        for (let k = 0; k < values.length; k += 1) {
+          const normalised_value = (((values[k] - freq_min) * 2) / (freq_max - freq_min)) - 1;
+          const normalised_color = Color(random_color).whiten(normalised_value).hex();
+
+          result.push({ name: major[k], parent: fac, value: values[k], color: random_color });
+        }
+      }
+      return result;
     },
   },
   firebase: {
---
+++ src/components/charts/treemap.vue
@@ -0,0 +1,60 @@
+<template>
+  <highcharts :options="options" ref="treemap" class="tree"></highcharts>
+</template>
+
+<script>
+import HighchartsVue from 'highcharts-vue';
+import loadTreemap from 'highcharts/modules/treemap';
+import Highcharts from 'highcharts';
+
+loadTreemap(Highcharts);
+
+export default {
+  components: {
+    HighchartsVue,
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    options() {
+      return {
+        series: [{
+          type: 'treemap',
+          layoutAlgorithm: 'stripes',
+          alternateStartingDirection: true,
+          levels: [{
+            level: 1,
+            layoutAlgorithm: 'sliceAndDice',
+            dataLabels: {
+              enabled: true,
+              align: 'left',
+              verticalAlign: 'top',
+              style: {
+                fontSize: '15px',
+                fontWeight: 'bold',
+              },
+            },
+          }],
+          data: this.data,
+        }],
+        title: {
+          text: 'Major and Faculty Breakdown',
+        },
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.line{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+</style>
