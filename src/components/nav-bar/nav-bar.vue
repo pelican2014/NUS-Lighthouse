6934453266b143f3ef7708a37d499a8ae4134f9d
Tongyao <312404930zhu@gmail.com>
2019-03-31 20:51:09 +0800

    changed nav-bar, comment out

--- src/components/nav-bar/nav-bar.vue
+++ src/components/nav-bar/nav-bar.vue
@@ -4,17 +4,7 @@
 export default {
   name: 'nav-bar',
   methods: {
-    update_recommended() {
-      let xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          alert(this.responseText);
-        }
-      };
-      xhttp.open("GET", "https://n8cdcjqay1.execute-api.us-east-1.amazonaws.com/default/SOMEfunciton", true);
-      xhttp.send();
-      alert(this.responseText);
-    },
+
 
     // var request = new XMLHttpRequest()
     // request.open('POST', 'https://n8cdcjqay1.execute-api.us-east-1.amazonaws.com/default/SOMEfunciton', true)
